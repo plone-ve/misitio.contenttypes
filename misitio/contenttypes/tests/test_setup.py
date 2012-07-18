3015806d2ac7c9cc2cfa54d68e744c498fffe80a
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-07-18 11:38:44 -0430

    Added unit test for content types

--- misitio/contenttypes/tests/test_setup.py
+++ misitio/contenttypes/tests/test_setup.py
@@ -2,12 +2,17 @@
 
 import unittest2 as unittest
 
+from Products.CMFCore.utils import getToolByName
+
+from plone.i18n.normalizer import idnormalizer
+
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 
 from misitio.contenttypes.config import PROJECTNAME
 from misitio.contenttypes.testing import INTEGRATION_TESTING
 
+from AccessControl import Unauthorized
 
 class InstallTestCase(unittest.TestCase):
 
@@ -15,10 +20,44 @@ class InstallTestCase(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
+        self.qi = getToolByName(self.portal, 'portal_quickinstaller')
+        self.types = getToolByName(self.portal, 'portal_types')
 
     def test_installed(self):
-        qi = getattr(self.portal, 'portal_quickinstaller')
-        self.assertTrue(qi.isProductInstalled(PROJECTNAME))
+        """ Validate that our products GS profile has been run and the product 
+            installed
+        """
+        self.assertTrue(self.qi.isProductInstalled(PROJECTNAME),
+                        'package appears not to have been installed')
+    
+    def test_contenttypes_are_registred(self):
+        """ Validate that our contenttypes are registred
+        """
+        existing = self.types.objectIds()
+        self.assertTrue('consejo_comunal' in existing)
+        self.assertTrue('miembro' in existing)
+
+    def test_consejo_comunal_allowed_content_types(self):
+
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        cc_title = 'Las piedritas'
+        oid = idnormalizer.normalize(cc_title, 'es')
+        #self.cc = createConcejoComunal(self.portal, cc_title)
+        self.portal.invokeFactory('consejo_comunal', id=oid, title=cc_title)
+        self.cc = self.portal[oid]
+        types = ['File', 'Image', 'Link', 'miembro',]
+        allowed_types = [t.getId() for t in self.cc.allowedContentTypes()]
+        for t in types:
+            self.assertTrue(t in allowed_types)
+        
+        # trying to add any other content type raises an error
+        self.assertRaises(ValueError,
+                          self.cc.invokeFactory, 'Document', 'Registro legal')
+        try:
+            miembro_title = 'Leonardo J. Caballero G.'
+            self.cc.invokeFactory('miembro', miembro_title)
+        except Unauthorized:
+            self.fail()
 
 class UninstallTest(unittest.TestCase):
 
@@ -27,7 +66,7 @@ class UninstallTest(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.qi = getattr(self.portal, 'portal_quickinstaller')
+        self.qi = getToolByName(self.portal, 'portal_quickinstaller')
         self.qi.uninstallProducts(products=[PROJECTNAME])
 
     def test_uninstalled(self):
